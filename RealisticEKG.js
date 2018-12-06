73dc61f6eeaf130b0f791a8f00a40416f96e17c0
Lksfnd <fendlukas@pm.me>
2018-12-07 00:17:40 +0100

    V1

--- RealisticEKG.js
+++ RealisticEKG.js
@@ -1,3 +1,6 @@
+// overlay functions have one argument: ratio, it is a value between 0 and 1, depending on the
+// percentage completetd until the next phase of the ekg starts
+
 let config = {
     // General
     speedMultiplicator: 1.2,
@@ -45,48 +48,39 @@ let config = {
         // Derivation durations
         duration: 150
     },
-    d_II: {
-        // Sections
+
+    d_V1: {
         pWave: {
-            width: -0.12,
-            height: -5,
-            elevation: 0
+            amplitude: 7,
+            duration: 20,
+            overlayFunction: ratio => { return 0; }
         },
-
-        pqTime: {
-            elevation: 0
+        pqSegment: {
+            duration: 17.5,
+            overlayFunction: ratio => { return 0; }
         },
-
-        qWave: {
-            pitch: -1.5,
-            offset: 0,
-            elevation: 0
+        qTooth: {
+            duration: 4,
+            amplitude: -8
         },
-
-        rWave: {
-            pitch: 5,
-            offset: -5,
-            elevation: 0
+        rTooth: {
+            duration: 10,
+            amplitude: 75
         },
-
-        sWave: {
-            pitch: -8,
-            offset: -2,
-            elevation: 0
+        sTooth: {
+            duration: 6,
+            amplitude: -18
         },
-
-        stTime: {
-            elevation: 0
+        stSegment: {
+            duration: 20,
+            overlayFunction: ratio => { return 0; }
         },
-
         tWave: {
-            width: 0.12,
-            height: -8,
-            elevation: 0
+            amplitude: 14,
+            duration: 40,
+            overlayFunction: ratio => { return 0; }
         },
-
-        // Derivation durations
-        duration: 150
+        completeOverlay: ratio => { return 0; }
     }
 };
 
@@ -95,106 +89,76 @@ class RealisticEKG {
 Times are in milliseconds (ms)
 
 */
-    constructor() {
-    }
+    constructor() {}
 
-
+    // duration: 200 ms (20 squares, 1 sq = 1 ms)
     d_I(x) {
-        const cfg = config.d_I;
-        x %= cfg.duration;
-        x *= config.speedMultiplicator;
+        const cfg = config.d_V1;
+        x %= 200;
+        const status = x / 200;
 
-        // P wave, duration: 27ms
-        if( x > 0 && x < 27) {
-            return (cfg.pWave.elevation + Math.sin( cfg.pWave.width * x ) * cfg.pWave.height);
+        // P Wave
+        // Duration: 20 ms
+        if( x < cfg.pWave.duration ) {
+            return -((Math.sin((x/cfg.pWave.duration)*Math.PI)*cfg.pWave.amplitude) + cfg.pWave.overlayFunction(x/cfg.pWave.duration)) - cfg.completeOverlay(status);
+        }
+        
+        // PQ Segment
+        // Duration: 17.5ms
+        let stepTime = x - cfg.pWave.duration;
+        if( stepTime < cfg.pqSegment.duration ) {
+            return -cfg.pqSegment.overlayFunction( stepTime / cfg.pqSegment.duration )- cfg.completeOverlay(status);
         }
 
-        // PQ time, duration: 16ms
-        else if( x > 27 && x < 43) {
-            return cfg.pqTime.elevation;
+        // Q-Tooth
+        stepTime = stepTime - cfg.pqSegment.duration;
+        if( stepTime < cfg.qTooth.duration ) {
+            return -( (stepTime / cfg.qTooth.duration) * cfg.qTooth.amplitude )- cfg.completeOverlay(status);
         }
 
-        // Q-Wave, duration: 6ms
-        else if( x > 43 && x < 49) {
-            return (x%43) * cfg.qWave.pitch + cfg.qWave.offset + cfg.qWave.elevation;
+        // R-Tooth
+        stepTime = stepTime - cfg.qTooth.duration;
+
+        // Rising part
+        if( stepTime < cfg.rTooth.duration*0.5 ) {
+            const amplitudeDifference = Math.abs( cfg.qTooth.amplitude ) + Math.abs( cfg.rTooth.amplitude );
+            const pitchPerMillisecond = amplitudeDifference / (cfg.rTooth.duration*0.5);
+            return -( cfg.qTooth.amplitude + ((stepTime+1 / cfg.rTooth.duration*0.5 ) * pitchPerMillisecond) )- cfg.completeOverlay(status);
         }
 
-        // R-Wave, duration: 9ms
-        // UP PART
-        else if( x > 49 && x < 58 ) {
-            return (x%49) * cfg.rWave.pitch + cfg.rWave.offset + cfg.rWave.elevation;
+        // Falling part  --> from R tooth to S tooth
+        stepTime = stepTime - cfg.rTooth.duration*0.5;
+        if( stepTime < cfg.rTooth.duration*0.5 ) {
+            const amplitudeDifference = Math.abs( cfg.rTooth.amplitude ) + Math.abs( cfg.sTooth.amplitude );
+            const pitchPerMillisecond = amplitudeDifference / (cfg.rTooth.duration*0.5);
+            return -( cfg.rTooth.amplitude - ((stepTime+1 / cfg.sTooth.duration ) * pitchPerMillisecond) )- cfg.completeOverlay(status);
         }
 
-        // S-Wave, duration: 7ms
-        // Downward phase, 3 ms
-        else if( x > 58 && x < 61) {
-            return (x%58) * cfg.sWave.pitch + cfg.sWave.offset + cfg.sWave.elevation;
-        }
-        // Upward phase, 4 ms
-        else if( x > 61 && x < 66) {
-            return (x%61) * (cfg.sWave.pitch*-0.09) + cfg.sWave.offset + cfg.sWave.elevation;
+
+        // S-Tooth reset to 0
+        stepTime = stepTime - cfg.rTooth.duration*0.5;
+
+        if( stepTime < cfg.sTooth.duration ) {
+            const amplitudeDifference = Math.abs( cfg.sTooth.amplitude );
+            const pitchPerMillisecond = amplitudeDifference / (cfg.sTooth.duration);
+            return -( cfg.sTooth.amplitude + ((stepTime+1 / cfg.sTooth.duration ) * pitchPerMillisecond) )- cfg.completeOverlay(status);
         }
 
-        else if( x > 66 && x < 88) {
-            return cfg.stTime.elevation;
+        // ST-Segment
+        stepTime = stepTime - cfg.sTooth.duration;
+        if( stepTime < cfg.stSegment.duration ) {
+            return -cfg.stSegment.overlayFunction( stepTime / cfg.stSegment.duration )- cfg.completeOverlay(status);
         }
 
-        // T wave, duration: 27ms
-        else if( x > 88 && x < 115) {
-            return (cfg.tWave.elevation + Math.sin( cfg.tWave.width * (x%86) ) * cfg.tWave.height);
-        } else {
-            return 0;
+        stepTime = stepTime - cfg.stSegment.duration;
+        if (stepTime < cfg.tWave.duration) {
+            return -((Math.sin((stepTime/cfg.tWave.duration)*Math.PI)*cfg.tWave.amplitude) + cfg.tWave.overlayFunction(x/cfg.tWave.duration))- cfg.completeOverlay(status);
         }
 
+        stepTime = stepTime - cfg.tWave.duration;
+        return - cfg.completeOverlay(status);
 
     }
 
-    d_II(x) {
-        const cfg = config.d_II;
 
-        x %= cfg.duration;
-        x *= config.speedMultiplicator;
-
-        // P wave, duration: 27ms
-        if( x > 0 && x < 27) {
-            return (cfg.pWave.elevation + Math.sin( cfg.pWave.width * x ) * cfg.pWave.height);
-        }
-
-        // PQ time, duration: 16ms
-        else if( x > 27 && x < 43) {
-            return cfg.pqTime.elevation;
-        }
-
-        // Q-Wave, duration: 6ms
-        else if( x > 43 && x < 49) {
-            return (x%43) * cfg.qWave.pitch + cfg.qWave.offset + cfg.qWave.elevation;
-        }
-
-        // R-Wave, duration: 9ms
-        // UP PART
-        else if( x > 49 && x < 58 ) {
-            return (x%49) * cfg.rWave.pitch + cfg.rWave.offset + cfg.rWave.elevation;
-        }
-
-        // S-Wave, duration: 7ms
-        // Downward phase, 3 ms
-        else if( x > 58 && x < 61) {
-            return (x%58) * cfg.sWave.pitch + cfg.sWave.offset + cfg.sWave.elevation;
-        }
-        // Upward phase, 4 ms
-        else if( x > 61 && x < 66) {
-            return (x%61) * (cfg.sWave.pitch*-0.09) + cfg.sWave.offset + cfg.sWave.elevation;
-        }
-
-        else if( x > 66 && x < 88) {
-            return cfg.stTime.elevation;
-        }
-
-        // T wave, duration: 27ms
-        else if( x > 88 && x < 115) {
-            return (cfg.tWave.elevation + Math.sin( cfg.tWave.width * (x%86) ) * cfg.tWave.height);
-        } else {
-            return 0;
-        }
-    }
 }
--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let f = new EKGFunction();
 let rekg = new RealisticEKG();
 
 f.derivationFunctions.d_I = rekg.d_I;
-f.derivationFunctions.d_II = rekg.d_II;
 
 
 
